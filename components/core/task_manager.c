b5756437eeefaad2b0055c99ef605454e062002d
syaswanthkumar66-hash <syaswanthkumar66@gmail.com>
2026-05-26 13:06:09 +0530

    Update task_manager.c

--- components/core/task_manager.c
+++ components/core/task_manager.c
@@ -11,35 +11,40 @@
 
 static const char *TAG = "TASK_MGR";
 
-// Task handles
 static TaskHandle_t wifi_task_handle = NULL;
 static TaskHandle_t udp_task_handle = NULL;
 static TaskHandle_t mqtt_task_handle = NULL;
 static TaskHandle_t telemetry_task_handle = NULL;
 static TaskHandle_t diagnostic_task_handle = NULL;
 
-// Prototypes
-static void wifi_task(void *pvParameters);
-static void udp_task(void *pvParameters);
-static void mqtt_task(void *pvParameters);
-static void telemetry_task(void *pvParameters);
-static void diagnostic_task(void *pvParameters);
+static void wifi_task(void *pvParameters) {
+    wifi_manager_task();
+    vTaskDelete(NULL);
+}
+static void udp_task(void *pvParameters) {
+    udp_realtime_task();
+    vTaskDelete(NULL);
+}
+static void mqtt_task(void *pvParameters) {
+    mqtt_client_task();
+    vTaskDelete(NULL);
+}
+static void telemetry_task(void *pvParameters) {
+    telemetry_task_run();
+    vTaskDelete(NULL);
+}
+static void diagnostic_task(void *pvParameters) {
+    diagnostics_task();
+    vTaskDelete(NULL);
+}
 
 void task_manager_create_tasks(void)
 {
     ESP_LOGI(TAG, "Creating tasks...");
-
     xTaskCreatePinnedToCore(wifi_task, "wifi", 4096, NULL, 23, &wifi_task_handle, 0);
     xTaskCreatePinnedToCore(udp_task, "udp_rx", 4096, NULL, 22, &udp_task_handle, 1);
     xTaskCreatePinnedToCore(mqtt_task, "mqtt", 6144, NULL, 10, &mqtt_task_handle, 1);
     xTaskCreatePinnedToCore(telemetry_task, "telemetry", 3072, NULL, 5, &telemetry_task_handle, 1);
     xTaskCreatePinnedToCore(diagnostic_task, "diag", 3072, NULL, 1, &diagnostic_task_handle, 1);
-
     ESP_LOGI(TAG, "All tasks created.");
 }
-
-static void wifi_task(void *pvParameters) { wifi_manager_task(); }
-static void udp_task(void *pvParameters) { udp_realtime_task(); }
-static void mqtt_task(void *pvParameters) { mqtt_client_task(); }
-static void telemetry_task(void *pvParameters) { telemetry_task_run(); }
-static void diagnostic_task(void *pvParameters) { diagnostics_task(); }
