a2dd40049a14e2bce38532e153ebacbe73e83ee9
syaswanthkumar66-hash <syaswanthkumar66@gmail.com>
2026-05-26 10:33:24 +0530

    Create event_bus.h

---
+++ components/core/event_bus.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "esp_err.h"
+
+typedef struct {
+    uint32_t id;
+    int32_t data;
+} event_t;
+
+esp_err_t event_bus_init(void);
+esp_err_t event_bus_post(const event_t *evt);
+esp_err_t event_bus_receive(event_t *evt, TickType_t timeout);
