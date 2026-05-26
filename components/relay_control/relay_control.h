e7c473fcc90f713d4a33b53df9e1cdc359023403
syaswanthkumar66-hash <syaswanthkumar66@gmail.com>
2026-05-26 14:08:11 +0530

    Update relay_control.h

--- components/relay_control/relay_control.h
+++ components/relay_control/relay_control.h
@@ -7,4 +7,8 @@ void relay_control_set(uint8_t relay, bool on);
 void relay_control_toggle(uint8_t relay);
 void relay_state_save(void);
 void relay_state_load(void);
-void relay_apply_state(void);   // apply loaded state to GPIOs without NVS write
+void relay_apply_state(void);
+
+// Accessors to replace direct extern access
+void relay_state_set_all(const bool *states);
+void relay_state_get_all(bool *states_out);
