ec35f68fd3ed9428b1858da21e2d606ba6aa6aeb
syaswanthkumar66-hash <syaswanthkumar66@gmail.com>
2026-05-26 10:58:48 +0530

    Create telemetry.c

---
+++ components/telemetry/telemetry.c
@@ -0,0 +1,16 @@
+// components/telemetry/telemetry.c
+#include "telemetry.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "app_config.h"
+
+static const char *TAG = "TELEM";
+
+void telemetry_init(void) {}
+void telemetry_task_run(void) {
+    while(1) {
+        ESP_LOGI(TAG, "Telemetry: heap=%d RSSI=...", esp_get_free_heap_size());
+        vTaskDelay(pdMS_TO_TICKS(TELEMETRY_PERIOD_MS));
+    }
+}
