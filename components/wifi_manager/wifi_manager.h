1eecc3afce477a62a88247117c220c64baaea379
syaswanthkumar66-hash <syaswanthkumar66@gmail.com>
2026-05-26 13:08:16 +0530

    Update wifi_manager.h

--- components/wifi_manager/wifi_manager.h
+++ components/wifi_manager/wifi_manager.h
@@ -1,4 +1,7 @@
 #pragma once
+#include "freertos/event_groups.h"
+extern EventGroupHandle_t wifi_event_group;
+extern const int WIFI_CONNECTED_BIT;
 
 void wifi_manager_start(void);
 void wifi_manager_task(void);
