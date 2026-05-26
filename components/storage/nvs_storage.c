26e5679339f0aab592e6b089aeb996329c21e7e6
syaswanthkumar66-hash <syaswanthkumar66@gmail.com>
2026-05-26 11:02:37 +0530

    Create nvs_storage.c

---
+++ components/storage/nvs_storage.c
@@ -0,0 +1,8 @@
+// components/storage/nvs_storage.c
+#include "storage.h"
+#include "nvs_flash.h"
+#include "esp_log.h"
+
+void storage_init(void) {
+    ESP_LOGI("STOR", "Storage initialized.");
+}
