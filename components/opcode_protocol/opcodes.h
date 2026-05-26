3d5ba93d10bbf56130d29bb35e56d653cc802e84
syaswanthkumar66-hash <syaswanthkumar66@gmail.com>
2026-05-26 10:48:50 +0530

    Create opcodes.h

---
+++ components/opcode_protocol/opcodes.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define OPCODE_RELAY_ON     0x01
+#define OPCODE_RELAY_OFF    0x02
+#define OPCODE_TOGGLE       0x03
+#define OPCODE_ACK          0x04
+#define OPCODE_GET_STATE    0x05
+#define OPCODE_DIMMER       0x06
