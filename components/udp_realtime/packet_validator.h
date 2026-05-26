81f89e58e9a1395a2b2c4e30b6d6f031bbb5b4ca
syaswanthkumar66-hash <syaswanthkumar66@gmail.com>
2026-05-26 13:13:10 +0530

    Update packet_validator.h

--- components/udp_realtime/packet_validator.h
+++ components/udp_realtime/packet_validator.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <stdbool.h>
+#include <stdint.h>
 #include "session_manager.h"
 
+void packet_validator_init(void);
 bool packet_validator_check_rate_limit(void);
-int packet_process(const uint8_t *data, size_t len, session_t *session, uint8_t *plain_out);
+int packet_process(const uint8_t *data, size_t len, session_t *session,
+                   uint8_t *plain_out, size_t plain_out_max);
