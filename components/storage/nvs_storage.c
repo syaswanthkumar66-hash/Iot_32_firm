0cd597a5e04b54a9f33502b560fa5976aed10768
syaswanthkumar66-hash <syaswanthkumar66@gmail.com>
2026-05-26 12:38:17 +0530

    Update nvs_storage.c

--- components/storage/nvs_storage.c
+++ components/storage/nvs_storage.c
@@ -1,8 +1,15 @@
-// components/storage/nvs_storage.c
 #include "storage.h"
-#include "nvs_flash.h"
+#include "nvs.h"
 #include "esp_log.h"
 
-void storage_init(void) {
-    ESP_LOGI("STOR", "Storage initialized.");
+void storage_init(void)
+{
+    nvs_handle_t handle;
+    esp_err_t err = nvs_open("storage", NVS_READWRITE, &handle);
+    if (err != ESP_OK) {
+        ESP_LOGE("STOR", "Failed to open NVS namespace");
+    } else {
+        nvs_close(handle);
+        ESP_LOGI("STOR", "Storage namespace ready.");
+    }
 }
