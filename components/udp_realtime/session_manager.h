1143f1a492a80748a38bfa943225c74cebdfee03
syaswanthkumar66-hash <syaswanthkumar66@gmail.com>
2026-05-26 10:45:37 +0530

    Create session_manager.h

---
+++ components/udp_realtime/session_manager.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <stdint.h>
+#include <stdbool.h>
+
+#define SESSION_KEY_LEN 32
+typedef struct {
+    bool active;
+    uint32_t session_id;
+    uint8_t session_key[SESSION_KEY_LEN];
+    uint8_t local_nonce[12];
+    uint32_t last_counter;
+} session_t;
+
+session_t *session_find(uint32_t id);
+session_t *session_create(uint32_t id, const uint8_t *remote_nonce, size_t nonce_len);
+void session_destroy(uint32_t id);
