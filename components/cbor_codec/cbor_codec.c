8dc0979e3b935ad87ba5d67c36ecf1d19903f6ac
syaswanthkumar66-hash <syaswanthkumar66@gmail.com>
2026-05-26 12:29:12 +0530

    Update cbor_codec.c

--- components/cbor_codec/cbor_codec.c
+++ components/cbor_codec/cbor_codec.c
@@ -1,3 +1,15 @@
-// components/cbor_codec/cbor_codec.c
 #include "cbor.h"
-// trivial wrapper
+#include "tinycbor/cbor.h"
+
+void cbor_encode_telemetry(uint8_t *buffer, size_t *len)
+{
+    CborEncoder encoder;
+    cbor_encoder_init(&encoder, buffer, 256, 0);
+    // Example: encode a simple map
+    CborEncoder map;
+    cbor_encoder_create_map(&encoder, &map, 1);
+    cbor_encode_text_stringz(&map, "heap");
+    cbor_encode_int(&map, esp_get_free_heap_size());
+    cbor_encoder_close_container(&encoder, &map);
+    *len = cbor_encoder_get_buffer_size(&encoder, buffer);
+}
