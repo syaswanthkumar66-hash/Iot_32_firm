ee74cc37ea77d757d5f679f0fa5c10e620a49bd0
syaswanthkumar66-hash <syaswanthkumar66@gmail.com>
2026-05-26 13:14:39 +0530

    Update opcode_parser.c

--- components/opcode_protocol/opcode_parser.c
+++ components/opcode_protocol/opcode_parser.c
@@ -8,19 +8,13 @@ void opcode_dispatch(uint8_t opcode, const uint8_t *data, size_t len)
 {
     switch (opcode) {
         case OPCODE_RELAY_ON:
-            if (len >= 1 && data[0] < RELAY_COUNT)
-                relay_control_set(data[0], true);
+            if (len >= 1 && data[0] < RELAY_COUNT) relay_control_set(data[0], true);
             break;
         case OPCODE_RELAY_OFF:
-            if (len >= 1 && data[0] < RELAY_COUNT)
-                relay_control_set(data[0], false);
+            if (len >= 1 && data[0] < RELAY_COUNT) relay_control_set(data[0], false);
             break;
         case OPCODE_TOGGLE:
-            if (len >= 1 && data[0] < RELAY_COUNT)
-                relay_control_toggle(data[0]);
-            break;
-        case OPCODE_GET_STATE:
-            // Response will be implemented later
+            if (len >= 1 && data[0] < RELAY_COUNT) relay_control_toggle(data[0]);
             break;
         default:
             ESP_LOGW("OPCODE", "Unknown opcode 0x%02x", opcode);
