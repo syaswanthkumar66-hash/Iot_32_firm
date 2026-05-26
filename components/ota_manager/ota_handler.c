cb41837f0e8eedf4bf0b8ce3f0b437a2478dff3a
syaswanthkumar66-hash <syaswanthkumar66@gmail.com>
2026-05-26 12:36:52 +0530

    Update ota_handler.c

--- components/ota_manager/ota_handler.c
+++ components/ota_manager/ota_handler.c
@@ -1,9 +1,8 @@
-// components/ota_manager/ota_handler.c
 #include "ota_manager.h"
 #include "esp_log.h"
 
 static const char *TAG = "OTA";
 
 void ota_manager_init(void) {
-    ESP_LOGI(TAG, "OTA ready (Ed25519 verification)");
+    ESP_LOGI(TAG, "OTA ready (Ed25519 verification).");
 }
