798ad0aaabc84b3c9970b37a313985401e6ed408
syaswanthkumar66-hash <syaswanthkumar66@gmail.com>
2026-05-26 13:11:54 +0530

    Update session_manager.h

--- components/udp_realtime/session_manager.h
+++ components/udp_realtime/session_manager.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <stdint.h>
 #include <stdbool.h>
-#include "security.h" // for SESSION_KEY_LEN, DEVICE_TOKEN_LEN
+#include "security.h"
 
 typedef struct {
     bool active;
@@ -11,6 +11,6 @@ typedef struct {
     uint32_t last_counter;
 } session_t;
 
-session_t *session_find(uint32_t id);
+__attribute__((warn_unused_result)) session_t *session_find(uint32_t id);
 session_t *session_create(uint32_t id, const uint8_t *remote_nonce, size_t nonce_len);
 void session_destroy(uint32_t id);
