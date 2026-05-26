bd881e390a4cf4a0700f2a17751ad7e9e2f645dc
syaswanthkumar66-hash <syaswanthkumar66@gmail.com>
2026-05-26 13:15:55 +0530

    Update relay_control.h

--- components/relay_control/relay_control.h
+++ components/relay_control/relay_control.h
@@ -1,8 +1,10 @@
 #pragma once
 #include <stdbool.h>
+#include <stdint.h>
 
 void relay_control_init(void);
 void relay_control_set(uint8_t relay, bool on);
 void relay_control_toggle(uint8_t relay);
 void relay_state_save(void);
 void relay_state_load(void);
+void relay_apply_state(void);   // apply loaded state to GPIOs without NVS write
